0027c874c3cb379f37488405b8103056cbb3eac7
zudevon <devon.rasch@degreeanalytics.com>
2018-11-26 01:47:17 -0600

    day 2 of some django

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add thumbnail later
+    # add author later
+
+    def __str__(self):
+        return self.title
+
+
+    def snippet(self):
+        return self.body[:50]+'...'  # returns only the first 50 characters
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Articles
 
 # Create your views here.
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Articles.objects.all().order_by("date")
+    return render(request, 'articles/article_list.html', {'articles': articles})
