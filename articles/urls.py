1612a4c4bf3d1194a2e4a3cff93c9055c8e24cb1
zudevon <devon.rasch@degreeanalytics.com>
2018-11-28 00:15:37 -0600

    Day 3

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from . import views
 from django.conf.urls import url
 
+
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.article_list),
+    url(r'^$', views.article_list, name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail')  # Regrex format... easier in Django2
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Articles
+from django.http import HttpResponse
 
 # Create your views here.
 def article_list(request):
     articles = Articles.objects.all().order_by("date")
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    return HttpResponse
